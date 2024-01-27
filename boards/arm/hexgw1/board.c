7c2d8b3c6b61a121394c33b28d2501c7bddd3890
Grzegorz Blach <grzegorz@blach.pl>
2024-01-27 21:20:42 +0100

    Update board files to zephyr 3.5

--- boards/arm/hexgw1/board.c
+++ boards/arm/hexgw1/board.c
@@ -7,26 +7,32 @@
 #include <zephyr/init.h>
 #include <hal/nrf_power.h>
 
-static int board_init(const struct device *dev) {
-    ARG_UNUSED(dev);
+static int board_init(void)
+{
+	if ((nrf_power_mainregstatus_get(NRF_POWER) ==
+	     NRF_POWER_MAINREGSTATUS_HIGH) &&
+	    ((NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) ==
+	     (UICR_REGOUT0_VOUT_DEFAULT << UICR_REGOUT0_VOUT_Pos))) {
 
-    if((nrf_power_mainregstatus_get(NRF_POWER) == NRF_POWER_MAINREGSTATUS_HIGH) &&
-    	((NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) == (UICR_REGOUT0_VOUT_DEFAULT << UICR_REGOUT0_VOUT_Pos))) {
+		NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
+		while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+			;
+		}
 
-        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
-        while(NRF_NVMC->READY == NVMC_READY_READY_Busy) { ; }
+		NRF_UICR->REGOUT0 =
+		    (NRF_UICR->REGOUT0 & ~((uint32_t)UICR_REGOUT0_VOUT_Msk)) |
+		    (UICR_REGOUT0_VOUT_3V0 << UICR_REGOUT0_VOUT_Pos);
 
-        NRF_UICR->REGOUT0 =
-            (NRF_UICR->REGOUT0 & ~((uint32_t)UICR_REGOUT0_VOUT_Msk)) |
-            (UICR_REGOUT0_VOUT_3V0 << UICR_REGOUT0_VOUT_Pos);
+		NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+		while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+			;
+		}
 
-        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
-        while(NRF_NVMC->READY == NVMC_READY_READY_Busy) { ; }
+		/* a reset is required for changes to take effect */
+		NVIC_SystemReset();
+	}
 
-        NVIC_SystemReset();
-    }
-
-    return 0;
+	return 0;
 }
 
 SYS_INIT(board_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
